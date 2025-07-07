a08fb38747192b8929a47c4599eb461aa7b8910b
Birewon <pranikp52@gmail.com>
2025-07-07 20:47:57 +0300

    The GuessTheNum bot can be using by multiple users

--- GuessTheNum/backend.py
+++ GuessTheNum/backend.py
@@ -5,19 +5,25 @@ from random import randint
 router = Router()
 
 ATTEMPTS = 5
-data = {'in_game': False,
-        'picked_num': None,
-        'attempts': None,
-        'total_games': 0,
-        'wins': 0}
+
+users = {}
 
 def get_random_number() -> int:
     return randint(1, 100)
 
 
-@router.message(CommandStart())
+@router.message(lambda x: x.text and x.text == '/start')
 async def start_handler(message: Message):
     await message.answer('Hi! Let\'s play the game!\nRules: I\'m guessing a number from 1 to 100 and you have to guess it. To start the game, enter the command /game or /play.')
+
+    if message.from_user.id not in users:
+        users[message.from_user.id] = {
+            'in_game': False,
+            'picked_num': None,
+            'attempts': None,
+            'total_games': 0,
+            'wins': 0
+        }
 
 @router.message(Command('help'))
 async def help_handler(message: Message):
@@ -27,14 +33,14 @@ async def help_handler(message: Message):
 async def stat_handler(message: Message):
     await message.answer(
         f'Your stats:\n'
-        f'Wins - {data['wins']}\n'
-        f'Total games - {data['total_games']}'
+        f'Wins - {users[message.from_user.id]['wins']}\n'
+        f'Total games - {users[message.from_user.id]['total_games']}'
     )
 
 @router.message(Command('cancel'))
 async def leave_game(message: Message):
-    if data['in_game']:
-        data['in_game'] = False
+    if users[message.from_user.id]['in_game']:
+        users[message.from_user.id]['in_game'] = False
         await message.answer(
             'The game has been cancelled.\n'
             'You may to start the new game!'
@@ -48,15 +54,15 @@ async def leave_game(message: Message):
 
 @router.message(Command('game', 'play'))
 async def start_game(message: Message):
-    if data['in_game']:
+    if users[message.from_user.id]['in_game']:
         await message.answer(
             'You already have an active game!\n'
             'I can react only to /cancel and any number from 1 to 100'
         )
     else:
-        data['in_game'] = True
-        data['picked_num'] = get_random_number()
-        data['attempts'] = ATTEMPTS
+        users[message.from_user.id]['in_game'] = True
+        users[message.from_user.id]['picked_num'] = get_random_number()
+        users[message.from_user.id]['attempts'] = ATTEMPTS
         await message.answer(
             'Nice! I picked a number from 1 to 100.\n'
             'Try to guess it!'
@@ -64,30 +70,30 @@ async def start_game(message: Message):
 
 @router.message(lambda x: x.text and x.text.isdigit() and 1 <= int(x.text) <= 100)
 async def try_to_guess(message: Message):
-    if data['in_game']:
-        if data['picked_num'] == int(message.text):
-            data['in_game'] = False
-            data['total_games'] += 1
-            data['wins'] += 1
+    if users[message.from_user.id]['in_game']:
+        if users[message.from_user.id]['picked_num'] == int(message.text):
+            users[message.from_user.id]['in_game'] = False
+            users[message.from_user.id]['total_games'] += 1
+            users[message.from_user.id]['wins'] += 1
             await message.answer('Wow! You picked a right number! You win!')
-        elif data['picked_num'] > int(message.text):
-            data['attempts'] -= 1
+        elif users[message.from_user.id]['picked_num'] > int(message.text):
+            users[message.from_user.id]['attempts'] -= 1
             await message.answer(
                 'Oh no... The number I picked is bigger than your number\n'
-                f'Remaining attempts: {data['attempts']}'
+                f'Remaining attempts: {users[message.from_user.id]['attempts']}'
             )
         else:
-            data['attempts'] -= 1
+            users[message.from_user.id]['attempts'] -= 1
             await message.answer(
                 'Oh no... The number I picked is lower than your number\n'
-                f'Remaining attempts: {data['attempts']}'
+                f'Remaining attempts: {users[message.from_user.id]['attempts']}'
             )
-        if data['attempts'] == 0:
-            data['in_game'] = False
-            data['total_games'] += 1
+        if users[message.from_user.id]['attempts'] == 0:
+            users[message.from_user.id]['in_game'] = False
+            users[message.from_user.id]['total_games'] += 1
             await message.answer(
                 'Ha-ha! I won!\n'
-                f'The number I picked is {data["picked_num"]}'
+                f'The number I picked is {users[message.from_user.id]["picked_num"]}'
             )
     else:
         await message.answer('You didn\'t start the game!')
